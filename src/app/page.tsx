736a1dccde70fbae2b03601a89ce92855b1d90e1
nikola <nikodola@gmail.com>
2024-11-02 14:30:08 +0100

    portfolio item 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,21 +43,21 @@ export default function Home() {
         </ul>
       </div>
 
-    <div ref={appDescription} className=" mt-24 mx-auto text-center">
-      <div>
-        <h5 className="text-left text-gray-500">1.App Description</h5>
-        <h5 className="text-4xl text-left">Incident Counter</h5>
-        <h5 className="text-4xl text-left">Web App</h5>
-        <h5 className="text-left">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, 
-          sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna 
-          aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud 
-          exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, 
-          vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim 
-          qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
-        Lorem ipsum dolor sit amet, cons ectetuer adipiscing d</h5>
+    <div ref={appDescription} className=" mt-24 mx-auto text-center flex gap-24">
+      <div className="flex-1">
+        <h5 className="text-left text-gray-600 mb-4">1.App Description</h5>
+        <h5 className="text-4xl text-left text-pink-400">Incident Counter</h5>
+        <h5 className="text-4xl text-left text-pink-400 mb-8">Web App</h5>
+        <h5 className="text-left text-gray-300">Days Without Incident is a web application initially developed for Nik’s, a 
+          Swedish chocolate company, to promote safety and track incident-free days in the workplace.
+          The primary function of the app is to log and display the number of consecutive days without any incidents 
+          or accidents, helping teams celebrate safety milestones and monitor progress.
+          Beyond tracking incidents, Days Without Incident offers robust management features. Users can 
+          organize and oversee departments, employees, and incidents in one centralized platform. This makes it a valuable 
+          tool not only for safety tracking but also for maintaining an organized record of activities, 
+          ensuring a safer, more efficient work environment.</h5>
       </div>
-      
+      <Image className="flex-2" src={'/portfolio/niks/1.jpg'} width={600} height={0} alt="niks image"></Image>
     </div>
 
     </main>
