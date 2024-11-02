1f28706ac838cc2ffe24ead8f130817db580f516
nikola <nikodola@gmail.com>
2024-11-02 14:44:57 +0100

    vercel fix 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             Days Without Incident is a web application initially developed for Nik’s, a Swedish chocolate company, to promote safety and track incident-free days in the workplace. The primary function of the app is to log and display the number of consecutive days without any incidents or accidents, helping teams celebrate safety milestones and monitor progress. Beyond tracking incidents, Days Without Incident offers robust management features. Users can organize and oversee departments, employees, and incidents in one centralized platform. This makes it a valuable tool not only for safety tracking but also for maintaining an organized record of activities, ensuring a safer, more efficient work environment.
           </h5>
         </div>
-        <Image className="flex-2" src={'/portfolio/niks/1.jpg'} width={600} height={400} alt="niks image" />
+        <Image className="flex-2" src={'/portfolio/niks/1.jpg'} width={700} height={400} alt="niks image" />
       </div>
     </main>
   );
