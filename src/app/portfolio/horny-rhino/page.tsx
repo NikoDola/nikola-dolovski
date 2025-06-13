fa8c181a85a10013ccafd3d470ff95bbe67fccb6
nikola <nikodola@gmail.com>
2025-06-13 14:04:29 +0200

    Remove .ai and .psd files from tracking

---
+++ src/app/portfolio/horny-rhino/page.tsx
@@ -0,0 +1,22 @@
+
+
+export default function hornyRhino() {
+  return (
+    <section className="section-regular">
+      <h2>Love at First Site</h2>
+      <div className="folioTextImageWrapper">
+        <p>
+          I met Syda at the end of 2022. She was searching for someone to help
+          bring her vision for "Horny Rhino" to life. From the start, it was
+          clear this brand wasn’t just another label — it had heart. Horny Rhino
+          stands for quality, edge, and purpose. It’s a bold, eco-conscious
+          lifestyle brand that blends raw energy with a strong message. Every
+          product is crafted with care, built to last, and designed to make a
+          statement. Whether it's clothing, coffee, or handcrafted goods, Horny
+          Rhino reflects Syda’s passion and strength — a brand built to move
+          forward, just like her.
+        </p>
+      </div>
+    </section>
+  );
+}
