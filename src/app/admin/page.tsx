136c0b8ce86d11886d4d3c5d6825cf0d18325dd7
nikola <nikodola@gmail.com>
2025-06-27 13:21:14 +0200

    clean code

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,14 @@
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
+
+import Link from 'next/link'
 
 export default async function AdminPage() { // ← Note the async keyword
-  const cookieStore = await cookies()
-  const session = cookieStore.get('admin_session')?.value // ← No await needed here
-  
-  if (!session) {
-    redirect('/auth')
-  }
 
-  return <div>Admin Dashboard</div>
+
+  return (
+        <div>
+      <ul>
+        <Link href={"/admin/logo-inspiration"}>Logo Inspiration</Link>
+      </ul>
+    </div>
+  )
 }
