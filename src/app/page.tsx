b3a8e56914e3ab7965a728884674e97010d50c98
nikola <nikodola@gmail.com>
2025-11-03 02:35:13 +0100

    contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 import Skills from "@/components/Skills"
 import SocialLinks from "@/components/client/Sociallinks"
 import Loading from "@/components/ui/Loading"
+import ContactForm from "@/components/ContactForm"
 
 export default function Home() {
   const [isSkillsLoading, setSkillsLoading] = useState(true)
@@ -32,6 +33,7 @@ export default function Home() {
         <h2>Portfolio</h2>
         <p className="mb-8">More items coming soon!</p>
         <SocialLinks />
+        <ContactForm />
       </section>
     </main>
   )
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,32 +1,79 @@
-"use client"
+"use client";
+import "./ContactForm.css";
+import emailjs from "@emailjs/browser";
+import { useRef } from "react";
 
-import {  usePathname } from "next/navigation"
+export default function ContactForm() {
+  const form = useRef<HTMLFormElement | null>(null);
 
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!form.current) return;
 
-export default function ContactForm (){
-    const path = usePathname()
-    console.log(path)
-    return(
-        <>
-        <hr className="bg-gray-800 text-gray-800 opacity-25"/>
-          <section className="flex justify-around my-24 max-[1200px]:flex-col gap-8" >
-    
-            <div>
-            <h5 className="text-4xl font-bold mb-8">Have a project in mind?</h5>
-            <h5> I&apos;d love to hear from you. <br />
-                Drop a message, and let&apos;s make something amazing together!</h5>
-            </div>
-            <form className="flex flex-col gap-4 rounded-3xl-group focus:outline-red">
-                <div className="flex gap-4 max-[900px]:flex-col">
-                    <input className="w-full" name="name" placeholder="Your Name" />
-                    <input className=" w-full" name="name" placeholder="Your Email" />
-                </div>
-                
-                <textarea className=" h-40" name="body" placeholder="Your Message"/>
-                <button> Send</button>
-            </form>
-        </section>
-        </>
-      
-    )
+    emailjs
+      .sendForm(
+        "service_75xc7kn", // replace with your service ID
+        "template_5zdgufx", // replace with your template ID
+        form.current,
+        {
+          publicKey: "Qki0lVqKeYPFzsg1j",
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+  };
+  return (
+    <>
+      {" "}
+      <section className="ctfWrapper">
+        <div className="ctfTextWrapper">
+          <h2>Have a project in mind?</h2>
+          <h5>
+            {" "}
+            I&apos;d love to hear from you! <br />
+            Drop a message, and let&apos;s make something amazing together!
+          </h5>
+        </div>
+        <form className="ctfInputWrapper" ref={form} onSubmit={sendEmail}>
+          <div className="ctfNameLastnameWrapper">
+            <input className="ctfInput" name="name" placeholder="Your Name" />
+            <input className="ctfInput" name="name" placeholder="Your Email" />
+          </div>
+
+          <textarea className="ctfTextarea" name="message" placeholder="Your Message" />
+          <button className="ctfButton">Send</button>
+        </form>
+      </section>
+    </>
+  );
 }
+
+// <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
+// <script type="text/javascript">
+//     (function() {
+//         // https://dashboard.emailjs.com/admin/account
+//         emailjs.init({
+//           publicKey: "YOUR_PUBLIC_KEY",
+//         });
+//     })();
+// </script>
+// <script type="text/javascript">
+//     window.onload = function() {
+//         document.getElementById('contact-form').addEventListener('submit', function(event) {
+//             event.preventDefault();
+//             // these IDs from the previous steps
+//             emailjs.sendForm('contact_service', 'contact_form', this)
+//                 .then(() => {
+//                     console.log('SUCCESS!');
+//                 }, (error) => {
+//                     console.log('FAILED...', error);
+//                 });
+//         });
+//     }
+// </script>
